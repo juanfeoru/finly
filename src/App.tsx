e5d3fce94de9103c7a773202bfdd7900799a845b
juanfeoru <felipeortega383@gmail.com>
2026-09-25 14:55:23 -0500

    feat: persist transactions in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
 import AppLayout from "./layouts/AppLayout";
 import type { Transaction } from "./types/transaction";
 import { transactions as initialTransactions } from "./data/transactions";
 import { Route, Routes } from "react-router";
 import Dashboard from "./pages/Dashboard";
 import Transactions from "./pages/Transactions";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 function App() {
-  const [transactions, setTransactions] =
-    useState<Transaction[]>(initialTransactions);
+  const [transactions, setTransactions] = useLocalStorage<Transaction[]>(
+    "finly-transactions",
+    initialTransactions,
+  );
 
   return (
     <Routes>
